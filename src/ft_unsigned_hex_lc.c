c3f80290f87bdbbf15f2023eb6e75a222140ee4a
Sonya Patel <spatel@F3C003.42adel.org.au>
2022-07-25 13:18:08 +0930

    Commiting hex conversion

--- src/ft_unsigned_hex_lc.c
+++ src/ft_unsigned_hex_lc.c
@@ -6,34 +6,42 @@
 /*   By: spatel <spatel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 16:33:26 by spatel            #+#    #+#             */
-/*   Updated: 2022/07/22 17:44:06 by spatel           ###   ########.fr       */
+/*   Updated: 2022/07/25 13:17:32 by spatel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-void	ft_putnbr_hex(unsigned int n)
+void	ft_putnbr(int n)
 {
-	if (n == -2147483648)
-		write(1, "-2147483648", 12);
-	else if (n < 0)
+	int n;
+
+	if (n > 15)
 	{
-		write(1, '-', 1);
-		ft_putnbr(-n);
-	}
-	else if (n > 9)
-	{
-		ft_putnbr(n / 10);
-		write(1, n % 10 + '0', 1);
+		ft_putnbr(n / 16);
+		if (n % 16 > 9)
+			write(1, n % 16 + 87, 1);
+		else
+			write(1, n % 16 + '0', 1);
 	}
 	else
-		write(1, n + '0', 1);
+	{
+		if (n % 16 > 9)
+			write(1, n % 16 + 87, 1);
+		else
+			write(1, n % 16 + '0', 1);
+	}
 }
+
+
 
 int	ft_unsigned_hex_lc(unsigned int x);
 {
+	int	printed_bytes;
+	
 	//write unsigned int x
-	ft_unsigned_dec(x);
+	if (x)
+		ft_putnbr(x);
 	//calculate no.bytes printed
 
 }
